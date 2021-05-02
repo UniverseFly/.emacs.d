88a4fd41a18f6154b125224faa21e9920dc39d6d
Nole <universe_fly@icloud.com>
2021-05-02 23:32:42 +0800

    Add yasnippet and multiple-cursor support

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ivy helm-lsp swift-mode lsp-sourcekit company lsp-python-ms lsp-ui lsp-treemacs flycheck)))
+   '(multiple-cursors yasnippet lsp-ivy helm-lsp swift-mode lsp-sourcekit company lsp-python-ms lsp-ui lsp-treemacs flycheck)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -46,3 +46,5 @@
 (require-package 'helm-lsp)
 (require-package 'lsp-ivy)
 (require-package 'company)
+(require-package 'yasnippet)
+(require-package 'multiple-cursors)
