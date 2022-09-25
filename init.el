bb798d160a6560ef1c573f5953193fe0ad8efba1
Yuxiang Wei <yuxiang630.wei@gmail.com>
2022-09-25 15:35:25 -0500

    Disable backup/autosave

--- init.el
+++ init.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(markdown-command "pandoc -s --katex")
  '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(company-coq proof-general lsp-haskell haskell-mode multiple-cursors cmake-mode sbt-mode lsp-metals rust-mode lsp-treemacs treemacs counsel yasnippet swift-mode lsp-ui lsp-sourcekit lsp-pyright lsp-ivy flycheck company)))
+   '(meow god-mode company-coq proof-general lsp-haskell haskell-mode multiple-cursors cmake-mode sbt-mode lsp-metals rust-mode lsp-treemacs treemacs counsel yasnippet swift-mode lsp-ui lsp-sourcekit lsp-pyright lsp-ivy flycheck company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -159,3 +160,14 @@
 (require-package 'proof-general)
 (require-package 'company-coq)
 (add-hook 'coq-mode-hook #'company-coq-mode)
+
+;; evil
+(require-package 'evil)
+(evil-mode 1)
+
+(load-file (let ((coding-system-for-read 'utf-8))
+                (shell-command-to-string "agda-mode locate")))
+
+;; Do not backup nor auto save
+(setq make-backup-files nil)
+(setq auto-save-default nil)
